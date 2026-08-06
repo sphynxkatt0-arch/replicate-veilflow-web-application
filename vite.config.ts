f633714ca2d8ea5f7327813bdee4fb1250285d5c
sphynxkatt0-arch <sphynxkatt0@gmail.com>
2026-08-06 23:33:02 +0200

    Configure production-targeted Vite build

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [react()],
   server: { host: "127.0.0.1", port: 8080 },
+  build: {
+    target: "es2022",
+    sourcemap: true,
+  },
 });
