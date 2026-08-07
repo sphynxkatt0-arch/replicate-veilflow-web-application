ec79fca1f889f3b8db063270bbab5e06e9d73ca8
sphynxkatt0-arch <sphynxkatt0@gmail.com>
2026-08-07 15:15:24 +0200

    Prevent future blank-screen render failures

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./worldclass/mobilePerformanceBootstrap";
 import WorldclassApp from "./worldclass/App";
+import { AppErrorBoundary } from "./worldclass/AppErrorBoundary";
 import "./worldclass/platform.css";
 import "./worldclass/worldclass-ux.css";
 import "./worldclass/worldclass-layout.css";
@@ -9,4 +10,4 @@ import "./worldclass/mobile-performance.css";
 
 const root = document.getElementById("root");
 if (!root) throw new Error("Application root was not found");
-createRoot(root).render(<StrictMode><WorldclassApp /></StrictMode>);
+createRoot(root).render(<StrictMode><AppErrorBoundary><WorldclassApp /></AppErrorBoundary></StrictMode>);
